82770d8843c50be76793e9496a9ab39010df0428
Noah <noah.delgado@cpnv.ch>
2022-01-11 11:57:07 +0100

    Add Practice route and view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,9 @@
     {{-- TODO :changer le foreach par forealse --}}
     <div class="list-group">
         @foreach ($practices as $practice)
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+
+            <a href="/practice/{{ $practice->id }}"
+                class="list-group-item list-group-item-action flex-column align-items-start">
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">{{ $practice->domain->name }}</h5>
                     <small>{{ \Carbon\Carbon::parse($practice->updated_at)->formatLocalized('%d %B %Y') }}</small>
